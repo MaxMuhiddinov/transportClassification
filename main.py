e9f7df9b9c3f26c796a6a3e74a8b4551241f0f12
Azizjon <89641232+MaxAzizjon@users.noreply.github.com>
2022-06-09 12:42:30 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+#rasimni joylash
+
+file = st.file_uploader("Rasimni yuklash", type=['jfif','png', 'jpeg', 'gif', 'svg'])
+if file:
+    img = PILImage.create(file)
+    st.image(file)
+    model = load_learner('transport_model.pkl')
+
+
+
+    pred, pred_id, probs = model.predict(img)
+
+    st.success(f"Bashorat:{pred}")
+
+    st.info(f"Extimolik: {probs[pred_id]*100:.1f}%")
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
